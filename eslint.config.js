b374001e34ee94ed409d97430df97fa119b23c37
Elmer van Rooijen <e.van.rooijen@hotmail.com>
2025-04-12 12:38:36 +0200

    chore: Implement code changes to enhance functionality and improve performance

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  globalIgnores([".vinxi", ".output"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  eslintConfigPrettier,
+]);
